16de854bdc40d7a5971bb8d4d63a38f477060db4
Tiffany Kuo Chang <tiffanyk104@usf.edu>
2025-02-23 23:01:58 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -28,3 +28,27 @@ console.log(prod1.getDetails());
 
 prod1.updateStock(3);
 console.log(prod1.getDetails()); 
+
+// TASK 2: CREATING AN ORDER CLASS
+
+// Create order class containing order details
+class Order {
+    constructor(orderId, product, quantity) {
+        this.orderId = orderId;
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    getOrderDetails() {
+        let totalPrice = this.quantity * this.product.price;
+        let newStock = this.product.updateStock(this.quantity);
+
+        return `Order ID: ${this.orderId}, Product: ${this.product.name}, Quantity: ${newStock}, Total Price: $${totalPrice}`; 
+    }
+}
+
+// Test Case
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails());
+
+console.log(prod1.getDetails());
